dca64be4cb85b7aac7212eb7fe4120292543d9a4
cfynes14 <70376968+cfynes14@users.noreply.github.com>
2021-11-02 09:40:33 +0000

    render image in card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image } from 'react-native';
+import {View, StyleSheet, Text, Image, TouchableOpacity} from 'react-native';
 // import { StyleSheet, Link, Text } from 'react-router-dom';
 
-import { Hop } from '../../utils/interfaces';
+import {Hop} from '../../utils/interfaces';
 
 interface CardInterface {
   name: string;
@@ -16,32 +16,48 @@ interface CardInterface {
 }
 
 const Card = (props: CardInterface) => {
-  const styles = StyleSheet.create({
-    h3: {},
-    h4: {},
-    image: {
-      height: 100,
-      width: 50,
-    },
-  });
-
   return (
-    // <Link
-    //   to={`/details/${props.id}`}
-    //   className="card"
-    //   style={{ textDecoration: "none" }}
-    // >
-    <View>
-      <Text style={styles.h3}>{props.name}</Text>
-      {props.image ? (
-        <Image style={styles.image} source={{ uri: props.image }} />
-      ) : (
-        <Text style={styles.h4}>No Image </Text>
-      )}
-      <Text style={styles.h4}>{props.abv}%</Text>
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Text style={styles.h3}>{props.name}</Text>
+        <View style={styles.imageContainer}>
+          {props.image ? (
+            <Image style={styles.image} source={{uri: props.image}} />
+          ) : (
+            <Text style={styles.h4}>No Image </Text>
+          )}
+        </View>
+        <Text style={styles.h4}>{props.abv}%</Text>
+      </TouchableOpacity>
     </View>
-    // </Link>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 2,
+    marginHorizontal: 3,
+    alignItems: 'center',
+    height: 200,
+    width: 150,
+    borderWidth: 2,
+    borderColor: 'white',
+    borderRadius: 5,
+    flex: 1,
+  },
+  imageContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  h3: {color: 'white', fontFamily: 'IMFellEnglishSC-Regular', fontSize: 20},
+  h4: {color: 'white', fontSize: 15},
+  image: {
+    flex: 1,
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+  },
+});
 
 export default Card;
